33a42852e44f3b0e760a68156a64c36ae0699d4b
Erick Castañeda Martínez <erickce40@gmail.com>
2020-03-27 17:28:18 -0600

    knn working, missing menu

--- knnAlgorithm/distancias.py
+++ knnAlgorithm/distancias.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
-import numpy as np
 import csv
 import math
+
 class Point:
 
     def __init__(self, x, y, *family):
@@ -9,12 +9,10 @@ class Point:
         self.y = y
         self.family = family
 
-# O(1)
 def getEuclideanDistance(P1, P2):
     distance = math.sqrt(math.pow(P1.x - P2.x, 2) + math.pow(P1.y - P2.y, 2)) 
     return distance 
 
-# O(NlogN + N) -> O(NlogN)
 def getKNN(point, k, points):
     distances = []
     for i in range(0, len(points)):
@@ -22,11 +20,11 @@ def getKNN(point, k, points):
 
     distances = sorted(distances, key = lambda x: x[0])   
     
-    print('\nOrdered distances: \n')
-    for distance in distances:
-        print(distance)
+    # print('\nOrdered distances: \n')
+    # for distance in distances:
+    #     print(distance)
 
-    print('\nK nearest neighbours\n')
+    #print('\nK nearest neighbours\n')
     knns = []
     if len(distances) >= k:
         for i in range (0, k):
@@ -36,7 +34,6 @@ def getKNN(point, k, points):
         print('Use a smaller K value ')
         return []
 
-# O(N + NlogN) -> O(NlogN)
 def getfamily(distances):
     families = {}
     for i in range (0, len(distances)):    
@@ -54,7 +51,7 @@ def getfamily(distances):
 
     # return family
 
-def readFromCSV(fileName):
+def readPointsFromCSV(fileName):
     points = []
     with open(fileName) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -65,58 +62,75 @@ def readFromCSV(fileName):
                 line_count += 1
             else:
              #   print(f'\t ({row[0]}, {row[1]}) family: {row[2]}.')
-                points.append(Point(row[0], row[1], row[2]))
+                points.append(Point(float(row[0]), float(row[1]), row[2]))
                 line_count += 1
             #print(f'Processed {line_count} lines.')
     return points
 
-def addNewPoint(Point, points):
-    a = 10
+def readTestPointsFromCSV(fileName):
+    points = []
+    with open(fileName) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+              #  print(f'Column names are: {", ".join(row)}')
+                line_count += 1
+            else:
+             #   print(f'\t ({row[0]}, {row[1]}) family: {row[2]}.')
+                points.append(Point(float(row[0]), float(row[1])))
+                line_count += 1
+            #print(f'Processed {line_count} lines.')
+    return points    
+
+def addNewPoint(newPoint, points):
+    distances = getKNN(newPoint, 5, points)
+    newPoint.family = getfamily(distances)
+    return newPoint
+
+def checkNewPoint(newPoint, points):
+    distances = getKNN(newPoint, 5, points)
+    newPoint.family = getfamily(distances)
+    print(f'({newPoint.x}, {newPoint.y}) {newPoint.family[0]}')
 
 
-points = readFromCSV('datasheet.txt')
-testPoints = readFromCSV('testDataset.txt')
+if __name__ == "__main__":
 
-# p10 = Point(11, 20)
-# distances = getKNN(p10, 5, points)
-# for distance in distances:
-#     print(distance)
+    points = readPointsFromCSV('datasheet.txt')
+    testPoints = readTestPointsFromCSV('testDataset.txt')
 
-# p10.family = getfamily(distances)
-# points.append(p10)
-# for point in points:
-#     print(f'({point.x} , {point.y}) {point.family}')
+    print('\nPoints extracted from dataset: \n')
+    for point in points:
+        print(f'({point.x}, {point.y}) {point.family[0]}')
 
+    print('\nPoints added: \n')
+    for point in testPoints:
+        checkNewPoint(point, points)
 
+    # Los datos de esta línea para abajo son únicamente para graficar
 
+    oneX = []
+    oneY = []
+    fiveX = []
+    fiveY = []
+    sevenX = []
+    sevenY = []
 
+    for i in range(len(points)):
+        #print(f'{points[i].x} {points[i].y} {points[i].family[0]}')
+        if points[i].family[0] == '1':
+            oneX.append(float(points[i].x))
+            oneY.append(float(points[i].y))
+        elif points[i].family[0] == '5':
+            fiveX.append(float(points[i].x))
+            fiveY.append(float(points[i].y))
+        elif points[i].family[0] == '7':
+            sevenX.append(float(points[i].x))
+            sevenY.append(float(points[i].y))
 
-# Los datos de esta línea para abajo son únicamente para graficar
-
-oneX = []
-oneY = []
-fiveX = []
-fiveY = []
-sevenX = []
-sevenY = []
-
-for i in range(len(points)):
-    #print(f'{points[i].x} {points[i].y} {points[i].family[0]}')
-    if points[i].family[0] == '1':
-        oneX.append(float(points[i].x))
-        oneY.append(float(points[i].y))
-    elif points[i].family[0] == '5':
-        fiveX.append(float(points[i].x))
-        fiveY.append(float(points[i].y))
-    elif points[i].family[0] == '7':
-        sevenX.append(float(points[i].x))
-        sevenY.append(float(points[i].y))
-
-plt.plot(oneX, oneY, 'bo')
-plt.plot(fiveX, fiveY, 'go')
-plt.plot(sevenX, sevenY, 'ro')  
-plt.ylabel('Y')
-plt.xlabel('X')
-plt.show()
-
-
+    plt.plot(oneX, oneY, 'bo')
+    plt.plot(fiveX, fiveY, 'go')
+    plt.plot(sevenX, sevenY, 'ro')  
+    plt.ylabel('Erick Castañeda Martínez')
+    plt.xlabel('K - Nearest Neighbours')
+    #plt.show()
